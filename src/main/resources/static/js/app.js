06f58078014d9aadc290dc37abf2f8b511dc786b
kollanyid <kollanyid@protonmail.com>
2018-01-27 20:25:21 +0100

    Configured websocket, create html page to view ongoing messages, wire components together.

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,26 @@
+var stompClient = null;
+
+function connect() {
+    var socket = new SockJS('/websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/messages', function (message) {
+            showMessage(message.body);
+        });
+    });
+}
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+
+function showMessage(message) {
+    $("#messages").append("<tr><td>" + message + "</td></tr>");
+}
+
+
